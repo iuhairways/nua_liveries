508cc80b68cbe0ba1fe94b0b4fc6e9787501dbc5
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-09-06 17:19:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Loading...");
 let livObj;
- await fetch("https://raw.githubusercontent.com/iuhairways/nua_liveries/main/nua.js")
+ await fetch("https://raw.githubusercontent.com/iuhairways/nua_liveries/main/nua.json")
  .then(res => res.json())
  .then(data => livObj = data) 
 if (geofs.version == 2.9) { //should work with URLs and livery IDs now, legacy geofs compat.
