4d1dfa8b05a2192d39b24b3ead8f76eb6be7a287
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-09-06 17:40:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("Loading...");
 let livObj;
  await fetch("https://raw.githubusercontent.com/iuhairways/nua_liveries/main/nua.json")
- .then(res => res.json)
+ .then(res => res.json())
  .then(data => livObj = data) 
 if (geofs.version == 2.9) { //should work with URLs and livery IDs now, legacy geofs compat.
 livObj.aircraft.forEach(function(e){
@@ -54,23 +54,5 @@ document.querySelectorAll('[data-aircraft]').forEach(function(e){
     }
 });
 console.log("Loaded!");
-let contributors;
-await fetch("https://raw.githubusercontent.com/Spice9/Geofs-Multiliveries/main/dependencies/contributors.txt")
-.then(res => res.json())
- .then(data => contributors = data)
-let message = ""
-setTimeout(function(){
-  console.log("Code by Spice9 and AriakimTaiyo, livery contributions by:");
-contributors.forEach(function(e){
-  if (message === "") {
-    message = message + e
-  } else {
-    if (contributors[contributors.length - 1] === e) {
-      message = message + ", and " + e;
-    } else {
-      message = message + ", " + e
-    }
-  }
-})
-console.log(message)
+
 }, 1000)
